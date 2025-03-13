68e3824bd12ed7695a024ddd1ff97555770592a6
twburns88 <111901598+twburns88@users.noreply.github.com>
2025-03-13 16:35:31 -0500

    Added inference for bi and cross encoders and commented out test code for models

--- models/cross_encoder.py
+++ models/cross_encoder.py
@@ -2,9 +2,12 @@ from sentence_transformers import CrossEncoder
 model_path = "models/model_ce"
 model = CrossEncoder(model_path, num_labels=4, automodel_args={'ignore_mismatched_sizes': True})
 
+'''
+#Testing the model
 scores = model.predict([('carrots', 'Fresh-Cut Vegetable Tray and Ranch Dressing, priced per pound'), 
                         ('carrots', 'Sweet Potato & Carrot Recipe, Grain Free Dry Dog Food , 23.5 lbs.'), 
                         ('carrots', 'Carrot Bar Cake, 38 oz.'),
                         ('carrots', 'Whole Carrots, 5 lbs.'),
                         ('carrots', 'Blue Buffalo Nudges Homestyle Chicken, Peas, and Carrots Natural Dog Treats, 40 oz.')])
 print(scores)
+'''
